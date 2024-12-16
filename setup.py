9bda00728967bb251675e1c5e61465da1ec8fbfd
Sahas <sahasmcg2995@gmail.com>
2024-12-16 11:22:05 +0530

    Add initial configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fastapi-keycloak-auth',
+    version='1.0.0',
+    packages=['keycloak_auth'],
+    url='https://github.com/SahasPunchihewa',
+    license='',
+    author='sahas',
+    author_email='sahasmcg2995@gmail.com',
+    description='Keycloak authentication for FastAPI that helps to secure your API endpoints with RBAC with minimal configurations',
+)
